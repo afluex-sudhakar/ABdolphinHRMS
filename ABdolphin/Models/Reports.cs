a763f16f17f6b27dd7f2c72f85423fbd2a4e406b
Ashwani Mishra <developer6.afluex@gmail.com>
2023-09-20 15:28:05 +0530

    'changes'

---
+++ ABdolphin/Models/Reports.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ABdolphin.Models
+{
+    public class Reports
+    {
+        public string LoginId { get; set; }
+        public string UserName { get; set; }
+        public string LoginIDD { get; set; }
+
+
+        public DataSet GettingUserProfile()
+        {
+            SqlParameter[] para = {
+                                        new SqlParameter("@LoginId", LoginId)};
+            DataSet ds = Connection.ExecuteQuery("GetUserProfile", para);
+            return ds;
+        }
+    }
+}
